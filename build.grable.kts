70613cb91b079c2b1b04d7c3528887622d55d401
zarkol622-cmd <zarkol622@gmail.com>
2026-01-01 21:42:13 +0200

    Create build.grable.kts

---
+++ build.grable.kts
@@ -0,0 +1,10 @@
+// Top-level build file
+plugins {
+    id("com.android.application") version "8.4.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
