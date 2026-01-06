8dbda6dcc4e1c5e6467cfb98d6540384a5c7e301
zarkol622-cmd <zarkol622@gmail.com>
2026-01-06 19:47:32 +0200

    Update build.grable.kts

--- android/app/build.grable.kts
+++ android/app/build.grable.kts
@@ -83,4 +83,11 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+}dependencies {
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.0")
+    
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 }
